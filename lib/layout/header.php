59001b0a4d4561c78b08f4b5888a708f742720de
Johnny Rivera <johnny1864g@gmail.com>
2025-05-14 14:17:56 -0700

    fixed issue

--- lib/layout/header.php
+++ lib/layout/header.php
@@ -35,7 +35,7 @@
 
             <?php if(!empty($cta)):?>     
                 <a
-                    class="btn <?php if(!$isHome) : ?>btn--secondary<? endif; ?>"
+                    class="btn <?php if(!$isHome) : ?>btn--secondary<?php endif; ?>"
                     href="<?php echo esc_url( $cta['url'] ); ?>"
                     target="<?php echo esc_attr( $cta['target'] ); ?>"
                 >

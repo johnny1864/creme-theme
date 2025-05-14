4846b9423e22fe6a91278803b0e01d70e3bf989a
Johnny Rivera <johnny1864g@gmail.com>
2025-05-14 15:24:30 -0700

    updates

--- lib/case-study-blocks/full_width_media.php
+++ lib/case-study-blocks/full_width_media.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="full-width-media__wrapper <?php echo get_sub_field('aspect_ratio'); ?>">
             <?php if(get_sub_field('image')) : ?>
-                <?php echo getIMG(get_sub_field('image')['ID']) ?>
+                <?php echo getIMG(get_sub_field('image')['ID'], 'xl') ?>
             <?php elseif(get_sub_field('video')) : ?>
                 <video loading="lazy" autoplay loop muted playsinline>
                         <source

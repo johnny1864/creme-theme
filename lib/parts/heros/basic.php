411cf8cf17be9ee15378e2d9f0f8bb57f41efb60
Johnny Rivera <johnny1864g@gmail.com>
2025-05-14 15:08:55 -0700

    update hero

--- lib/parts/heros/basic.php
+++ lib/parts/heros/basic.php
@@ -1,13 +1,23 @@
 <section <?php echo $classes; echo $feat_img;?>>
     <div class="container">
+
+        <?php if(!empty($hero['gif'])):?>
+            <div class="hero__gif">
+                <?php echo getIMG($hero['gif']['ID'], 'md') ?>
+            </div>
+        <?php endif ?>
+
         <?php if(!empty($hero['preheading'])):?>
             <p class="preheading">
                 <?php echo $hero['preheading']; ?>
             </p>
         <?php endif; ?>
+        
         <h1 class="hero__title"><?php echo $title; ?></h1>
+
         <?php if(!empty($hero['content'])):?>
         <div class="hero__content h6"><?php echo $hero['content'];  ?></div>
         <?php endif; ?>
+
     </div>
 </section>
